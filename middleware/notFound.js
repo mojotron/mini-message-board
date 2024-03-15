35906911a47d3dc2a49b6cee49fe0cdcc5453b1c
mojotron <igormalj@gmail.com>
2024-03-15 19:55:46 +0100

    update file structure

--- middleware/notFound.js
+++ middleware/notFound.js
@@ -1,5 +1,5 @@
 const notFound = (req, res) => {
-  return res.status(404).send(`Route ${req.url} does not exist`);
+  return res.status(404).render('pages/notFound');
 };
 
 module.exports = notFound;
