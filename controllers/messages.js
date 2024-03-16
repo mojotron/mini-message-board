0507903a6e2411c5f6a99297f11c915e1fae2bad
mojotron <igormalj@gmail.com>
2024-03-16 20:52:32 +0100

    setup design config

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,28 +1,24 @@
 const messagesData = require("../data/messages");
 
 const getMessagesView = (req, res) => {
-  res
-    .status(200)
-    .render("pages/index", {
-      title: "Mini-Message-Board",
-      messages: messagesData,
-    });
+  res.status(200).render("pages/index", {
+    title: "Mini-Message-Board",
+    messages: messagesData,
+  });
 };
 
 const getMessagesFormView = (req, res) => {
-  res
-    .status(200)
-    .render("pages/form", {
-      title: "Mini-Message-Board",
-      messages: messagesData,
-    });
+  res.status(200).render("pages/form", {
+    title: "Mini-Message-Board",
+    messages: messagesData,
+  });
 };
 
 const createMessage = (req, res) => {
   const { username, message } = req.body;
   if (username && message) {
     messagesData.push({ username, message, createdAt: new Date() });
-    return res.status(201).redirect("/");
+    return res.status(201).redirect("/messages");
   }
   res.render("form", { msg: `username and message must be provided` });
 };
